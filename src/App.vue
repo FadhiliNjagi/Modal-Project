4c87ff595cde4bdba67acb8aa27ccfa6c18932fa
Fadhili Njagi <fadhilinjagi@gmail.com>
2021-07-22 17:05:48 +0300

    Finished Modal Project follow-along by Net Ninja

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <h1>{{ greeting }}</h1><br>
+      <button @click="toggleModal">Show Modal</button>
+      <button @click="toggleModal2">Show Modal 2</button>
+      <teleport to=".modals" v-if="showModal">
+          <Modal theme="salefgf" @close="toggleModal">
+              <h1>Sale! Sale! Sale!</h1>
+              <p>Don't miss out on the sale</p>
+              <template v-slot:links>
+                  <a href="#">Sign Up</a>
+                  <a href="#">Log in</a>
+                  <a href="#">About Us</a>
+              </template>
+          </Modal>
+      </teleport>
+      <div v-if="showModal2">
+          <Modal @close="toggleModal2">
+              <h1>Modal 2 Successfully Created</h1>
+              <p>Hooray! Cheers to us</p>
+          </Modal>
+      </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal },
+  data() {
+    return {
+        greeting: 'My First Vue App',
+        header: "Subscribe to our newsletter!",
+        text: "Lots of awesome content every week",
+        showModal: false,
+        showModal2: false
+    }
+  },
+  methods: {
+      handleClick() {
+          console.log(this.$refs.name)
+          this.$refs.name.classList.add('active')
+      },
+      toggleModal() {
+          this.showModal = !this.showModal
+      },
+      toggleModal2(){
+          this.showModal2 = !this.showModal2
+      }
+  },
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ red: theme === 'sale' }">
+            <slot>Default content</slot>
+            <div class="actions">
+                <slot name="links"><a href="#">Links go here</a></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['theme'],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        },
+    }
+</script>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal.red {
+        background-color: red;
+        color: white;
+    }
+    .modal .actions a {
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
+    }
+    .modal.red a {
+        color: white;
+    }
+</style>
